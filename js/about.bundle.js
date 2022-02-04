76333bb8148eef015949fbb713ffc7d8f55fd4dc
petk1234 <petkun8034@gmail.com>
2022-02-05 00:07:56 +0200

    Updates

---
+++ js/about.bundle.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/about.js ***!
+  \**********************/
+console.log('about');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ndWxwLWJvaWxlcnBsYXRlLy4vc3JjL2Fib3V0LmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLE9BQVosRSIsImZpbGUiOiJhYm91dC5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnYWJvdXQnKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
