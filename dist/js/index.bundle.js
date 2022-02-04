4d8305e3e8477afb17fa049bc0c647392c9c36e1
petk1234 <petkun8034@gmail.com>
2022-02-05 00:26:41 +0200

    /dist added

---
+++ dist/js/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('index');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ndWxwLWJvaWxlcnBsYXRlLy4vc3JjL2luZGV4LmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLE9BQVosRSIsImZpbGUiOiJpbmRleC5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaW5kZXgnKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
